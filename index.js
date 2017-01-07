0598ac2cbc9332e4afd9de782230fb76d2df62c5
Marcus Hill <marcushill12@gmail.com>
2017-01-06 19:14:13 -0600

    Add eslint and update to uses screeps-webpack-plugin

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const Dashboard = require('./lib/dashboard')
-const Screeps = require('./lib/screeps-api')
-const ModuleManager = require('./lib/module-manager')
+const Dashboard = require('./lib/dashboard');
+const Screeps = require('./lib/screeps-api');
+const ModuleManager = require('./lib/module-manager');
+
+function getWebpackConfig(config){
+  let obj;
+  if (config.webpackConfig && typeof config.webpackConfig == 'object'){
+    obj = config.webpackConfig;
+  } else {
+    obj = require(config.webpackConfig || process.env.SCREEPS_WEBPACK_CONFIG);
+  }
+  if(!obj.plugins){
+    obj.plugins = [];
+  }
+  return obj;
+}
 
 module.exports = (config = {}) => {
   config = Object.assign({
@@ -9,85 +22,86 @@ module.exports = (config = {}) => {
     branch: process.env.SCREEPS_BRANCH || 'default',
     ptr: process.env.SCREEPS_PTR || false,
     src: process.env.SCREEPS_SOURCE || process.cwd()
-  }, config)
+  }, config);
+  config.webpackConfig = getWebpackConfig(config);
 
-  const dashboard = new Dashboard()
-  const screeps = new Screeps(config)
-  const modules = new ModuleManager(config)
+  const dashboard = new Dashboard();
+  const screeps = new Screeps(config);
+  const modules = new ModuleManager(config);
 
-  dashboard.console.log('{#666-fg}> Connecting to screeps...{/}')
+  dashboard.console.log('{#666-fg}> Connecting to screeps...{/}');
 
   dashboard.on('console', (data) => {
     if (data === 'quit') {
-      process.exit(0)
+      process.exit(0);
     } else {
-      screeps.postToConsole(data)
+      screeps.postToConsole(data);
     }
-  })
+  });
 
   dashboard.on('quit', () => {
-    process.exit(0)
-  })
+    process.exit(0);
+  });
 
   screeps.on('connected', () => {
-    dashboard.console.log('{#666-fg}> Authentication successful!{/}')
-    dashboard.console.log('{#666-fg}> Type "quit" to exit.{/}')
-  })
+    dashboard.console.log('{#666-fg}> Authentication successful!{/}');
+    dashboard.console.log('{#666-fg}> Type "quit" to exit.{/}');
+  });
 
   screeps.on('result', (result) => {
     if (result !== 'undefined') {
-      dashboard.log(`{#ccc-fg}< ${result}{/}`)
+      dashboard.log(`{#ccc-fg}< ${result}{/}`);
     }
-  })
+  });
 
   screeps.on('console', (line) => {
-    const time = (new Date()).toTimeString().slice(0, 8)
+    const time = (new Date()).toTimeString().slice(0, 8);
     if (line.error) {
       const error = line.error.split('\n').map((line, index) => {
         if (index > 0) {
-          line = ' '.repeat(11) + line
+          line = ' '.repeat(11) + line;
         }
-        return line
-      }).join('\n')
-      dashboard.log(`{#92676e-fg}[${time}]{/} {#e79da7-fg}${error}{/}`)
+        return line;
+      }).join('\n');
+      dashboard.log(`{#92676e-fg}[${time}]{/} {#e79da7-fg}${error}{/}`);
     } else {
-      dashboard.log(`{#999-fg}[${time}]{/} {#ccc-fg}${line}{/}`)
+      dashboard.log(`{#999-fg}[${time}]{/} {#ccc-fg}${line}{/}`);
     }
-  })
+  });
 
   screeps.on('cpu', ({cpu, memory}) => {
-    const cpuUsage = Math.floor(cpu / screeps.user.cpu * 100)
-    const memoryUsage = Math.floor(memory / 2097152 * 100)
+    const cpuUsage = Math.floor(cpu / screeps.user.cpu * 100);
+    const memoryUsage = Math.floor(memory / 2097152 * 100);
     if (cpuUsage >= 66) {
-      dashboard.cpuStatus.style.bar.bg = 'red'
+      dashboard.cpuStatus.style.bar.bg = 'red';
     } else if (cpuUsage > 33 && cpuUsage < 66) {
-      dashboard.cpuStatus.style.bar.bg = 'yellow'
+      dashboard.cpuStatus.style.bar.bg = 'yellow';
     } else {
-      dashboard.cpuStatus.style.bar.bg = 'green'
+      dashboard.cpuStatus.style.bar.bg = 'green';
     }
-    dashboard.cpuStatusText.setContent(`{green-fg}CPU:{/}{|}${cpu}/${screeps.user.cpu}`)
-    dashboard.cpuStatus.setProgress(cpuUsage)
-    dashboard.memoryStatusText.setContent(`{green-fg}Memory:{/}{|}${Math.round(memory / 1024)} / 2048 KB{/center}`)
-    dashboard.memoryStatus.setProgress(memoryUsage)
-    dashboard.screen.render()
-  })
+    dashboard.cpuStatusText.setContent(`{green-fg}CPU:{/}{|}${cpu}/${screeps.user.cpu}`);
+    dashboard.cpuStatus.setProgress(cpuUsage);
+    dashboard.memoryStatusText.setContent(`{green-fg}Memory:{/}{|}${Math.round(memory / 1024)} / 2048 KB{/center}`);
+    dashboard.memoryStatus.setProgress(memoryUsage);
+    dashboard.screen.render();
+  });
 
   screeps.on('gclProgress', (gcl) => {
-    const progress = Math.round(gcl.progress / gcl.progressTotal * 100)
-    dashboard.gclStatusText.setContent(`{green-fg}GCL ${gcl.level}:{/}{|}${progress}%`)
-    dashboard.gclStatus.setProgress(progress)
-  })
+    const progress = Math.round(gcl.progress / gcl.progressTotal * 100);
+    dashboard.gclStatusText.setContent(`{green-fg}GCL ${gcl.level}:{/}{|}${progress}%`);
+    dashboard.gclStatus.setProgress(progress);
+  });
 
-  modules.on('change', (file) => {
-    dashboard.console.log(`{grey-fg}ModuleManager:{/} Transforming ${file}...`)
-  })
+  modules.on('change', () => {
+    dashboard.console.log(`{grey-fg}ModuleManager:{/} A change has been detected`);
+  });
 
-  modules.on('change', (file) => {
-    dashboard.console.log('{grey-fg}ModuleManager:{/} Upload successful!')
-  })
+  modules.on('upload', () => {
+    dashboard.console.log('{grey-fg}ModuleManager:{/} Upload successful!');
+  });
 
   modules.on('error', (err) => {
-    dashboard.console.log('Module manager encountered an error!')
-    dashboard.console.log(err)
-  })
-}
+    dashboard.console.log('Module manager encountered an error!');
+    dashboard.console.log(err);
+  });
+};
